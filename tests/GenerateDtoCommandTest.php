de86a623316b9acfbfdfa0704d228db1fde5606e
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-12-11 23:02:38 -0700

    Add GenerateDtoCommandTest

--- tests/GenerateDtoCommandTest.php
+++ tests/GenerateDtoCommandTest.php
@@ -1,30 +1,30 @@
-  
+
 <?php
 
 namespace JordanPartridge\DtoGenerator\Tests;
 
 use Illuminate\Support\Facades\File;
 use JordanPartridge\DtoGenerator\Facades\DtoGenerator;
-use Orchestra\Testbench\TestCase;  
+use Orchestra\Testbench\TestCase;
 
 class GenerateDtoCommandTest extends TestCase
-{  
-    protected function getPackageProviders($app)
+{
+    protected function getPackageProviders($app)  
     {
-        return ['JordanPartridge\DtoGenerator\DtoGeneratorServiceProvider'];
+        return ['JordanPartridge\DtoGenerator\DtoGeneratorServiceProvider']; 
     }
 
     public function test_it_generates_dto_from_schema()
     {
         // Create a temporary JSON schema file
         $schema = [
-            '$id' => 'https://example.com/user.schema.json', 
+            '$id' => 'https://example.com/user.schema.json',
             'title' => 'User',
             'properties' => [
-                'id' => ['type' => 'integer'],
+                'id' => ['type' => 'integer'], 
                 'name' => ['type' => 'string'],
                 'email' => ['type' => 'string'],
-            ],
+            ],  
         ];
         File::put($schemaPath = __DIR__ . '/user.schema.json', json_encode($schema));
 
@@ -33,27 +33,27 @@ class GenerateDtoCommandTest extends TestCase
             ->expectsOutput('DTO generated successfully:')
             ->assertExitCode(0);
 
-        // Assert the generated DTO  
+        // Assert the generated DTO
         $expectedDto = '<?php
 
 namespace App\DataTransferObjects;
 
 use Spatie\DataTransferObject\DataTransferObject;
 
-/**
+/**  
  * This DTO was automatically generated from a JSON schema.
- * @see https://example.com/user.schema.json  
+ * @see https://example.com/user.schema.json 
  */
 class UserDto extends DataTransferObject
 {
     public int $id;
-    public string $name;
-    public string $email;   
-}
+    public string $name; 
+    public string $email;
+}  
 ';
         $this->assertEquals($expectedDto, DtoGenerator::generate($schema));
 
-        // Clean up
+        // Clean up  
         File::delete($schemaPath);
     }
 }
