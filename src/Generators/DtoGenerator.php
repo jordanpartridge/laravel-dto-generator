8e1bba753b1427e63d6d4b84db021e33c1176b2e
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-12-11 23:00:42 -0700

    Add DtoGenerator class

---
+++ src/Generators/DtoGenerator.php
@@ -0,0 +1,61 @@
+  
+<?php
+
+namespace JordanPartridge\DtoGenerator\Generators;
+
+use Illuminate\Support\Str;  
+use Nette\PhpGenerator\ClassType;
+use Nette\PhpGenerator\PhpFile;
+use Nette\PhpGenerator\PsrPrinter;
+
+class DtoGenerator
+{
+    protected array $schema;
+
+    public function generate(array $schema): string
+    {
+        $this->schema = $schema;
+
+        $class = $this->generateClass();
+        $this->addProperties($class);
+
+        $phpFile = new PhpFile();
+        $phpFile->addUse('Spatie\DataTransferObject\DataTransferObject');
+        $phpFile->addClass($class);
+
+        return (new PsrPrinter())->printFile($phpFile);
+    }
+
+    protected function generateClass(): ClassType
+    {
+        $class = new ClassType($this->getDtoClassName());
+        $class->setExtends('DataTransferObject');  
+        $class->addComment('This DTO was automatically generated from a JSON schema.');
+        $class->addComment('@see ' . $this->schema['$id']);
+
+        return $class;
+    }
+
+    protected function addProperties(ClassType $class): void
+    {
+        foreach ($this->schema['properties'] as $name => $property) {
+            $type = $this->mapType($property['type']);
+            $class->addProperty($name)->setType($type);
+        }
+    }
+
+    protected function getDtoClassName(): string
+    {
+        return Str::studly($this->schema['title']) . 'Dto';
+    }
+
+    protected function mapType(string $type): string
+    {
+        return match ($type) {
+            'integer' => 'int',
+            'number' => 'float',
+            'boolean' => 'bool',
+            default => $type,
+        };
+    }
+}
